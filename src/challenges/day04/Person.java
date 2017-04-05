b0c1ddf9b989c5666d2a81725e36f03771b4c110
rx05sx <Gert-Jan.Bossink@ing.nl>
2017-04-06 00:45:16 +0200

    Day01 and Day04 finished

---
+++ src/challenges/day04/Person.java
@@ -0,0 +1,67 @@
+package challenges.day04;
+
+import java.util.*;
+
+/**
+ * Created by M01D703 on 6-4-2017.
+ * Task
+ * Write a Person class with an instance variable, age, and a constructor that takes an integer, initialAge, as a parameter.
+ * The constructor must assign initialAge to age after confirming the argument passed as initialAge is not negative;
+ * if a negative argument is passed as initialAge, the constructor should set age to 0 and print
+ * Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
+ *
+ * 1) yearPasses() should increase the age instance variable by 1.
+ * 2) amIOld() should perform the following conditional actions:
+ *       If age < 13, print You are young..
+ *       If age >= 13 and age <18, print You are a teenager..
+ *       Otherwise, print You are old..
+ * To help you learn by example and complete this challenge, much of the code is provided for you, but you'll be
+ * writing everything in the future. The code that creates each instance of your Person class is in the main method.
+ * Don't worry if you don't understand it all quite yet!
+ */
+
+public class Person {
+    private int age;
+
+    public Person(int initialAge) {
+        // Add some more code to run some checks on initialAge
+        if (initialAge < 0) {
+            System.out.println("Age is not valid, setting age to 0..");
+        }
+        age = initialAge < 0 ? 0: initialAge;
+    }
+
+    public void amIOld() {
+        // Write code determining if this person's age is old and print the correct statement:
+        if (age < 13) {
+            System.out.println("You are young..");
+        } else {
+            if (age >= 13 && age < 18) {
+                System.out.println("You are a teenager..");
+            } else {
+                System.out.println("You are old..");
+            }
+        }
+//        System.out.println(/*Insert correct print statement here*/);
+    }
+
+    public void yearPasses() {
+        // Increment this person's age.
+        age++;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int age = sc.nextInt();
+            Person p = new Person(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++) {
+                p.yearPasses();
+            }
+            p.amIOld();
+            System.out.println();
+        }
+        sc.close();
+    }
+}
